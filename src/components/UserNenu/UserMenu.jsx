ad2d36615a0a151c48eb47ed62a78f0ccc06b247
ssulphurr <ssulphurr@gmail.com>
2023-06-17 14:50:55 +0300

    adds styles to UserMenu

--- src/components/UserNenu/UserMenu.jsx
+++ src/components/UserNenu/UserMenu.jsx
@@ -1,6 +1,7 @@
 import { useDispatch, useSelector } from 'react-redux';
 import { logOut } from 'redux/auth/operations';
 import { getUser } from 'redux/auth/selectors';
+import css from './UserMenu.module.css';
 
 export default function UserMenu() {
   const user = useSelector(getUser);
@@ -12,9 +13,13 @@ export default function UserMenu() {
   };
 
   return (
-    <div>
-      <p>Welcome, {user.email}</p>
-      <button onClick={handleLogOut}>Log out</button>
+    <div className={css.wrapper}>
+      <p className={css.text}>
+        Welcome,<span> {user.email}</span>
+      </p>
+      <button className={css.btn} onClick={handleLogOut}>
+        Log out
+      </button>
     </div>
   );
 }

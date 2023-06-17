b5da415dcff2a0266c0e6b0af276304fb17843f3
ssulphurr <ssulphurr@gmail.com>
2023-06-17 13:45:05 +0300

    makes http requests work

--- src/redux/auth/operations.jsx
+++ src/redux/auth/operations.jsx
@@ -16,7 +16,7 @@ export const register = createAsyncThunk(
   'auth/register',
   async (credentials, thunkAPI) => {
     try {
-      const responce = await axios.post('/user/signup', credentials);
+      const responce = await axios.post('/users/signup', credentials);
       setAuthHeader(responce.data.token);
       return responce.data;
     } catch (error) {
@@ -31,7 +31,7 @@ export const logIn = createAsyncThunk(
   'auth/login',
   async (credentials, thunkAPI) => {
     try {
-      const responce = await axios.post('/user/login', credentials);
+      const responce = await axios.post('/users/login', credentials);
       setAuthHeader(responce.data.token);
       return responce.data;
     } catch (error) {
@@ -42,11 +42,11 @@ export const logIn = createAsyncThunk(
 
 // post user logout
 
-export const logOut = createAsyncThunk('auth/logout', async (_, thunkAPI) => {
-  try {
-    await axios.post('/user/logout');
-    clearAuthHeader();
-  } catch (error) {
-    return thunkAPI.rejectWithValue(error.message);
-  }
-});
+// export const logOut = createAsyncThunk('auth/logout', async (_, thunkAPI) => {
+//   try {
+//     await axios.post('/users/logout');
+//     clearAuthHeader();
+//   } catch (error) {
+//     return thunkAPI.rejectWithValue(error.message);
+//   }
+// });
--- src/redux/contactsAndFilter/operations.jsx
+++ src/redux/contactsAndFilter/operations.jsx
@@ -1,8 +1,6 @@
 import { createAsyncThunk } from '@reduxjs/toolkit';
 import axios from 'axios';
 
-axios.defaults.baseURL = 'https://64876792beba62972790962c.mockapi.io/';
-
 export const fetchContacts = createAsyncThunk(
   'contacts/fetchAll',
   async (_, thunkAPI) => {
